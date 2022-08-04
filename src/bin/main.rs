f9dd47e5b8a0d8d5b60d61b7cb188724334b207c
Artem Ermakov <nawarius1993@gmail.com>
2022-08-05 00:26:28 +0400

    Adds: Multiple threads implementation

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,16 +1,22 @@
 use std::{
     fs,
     io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream}, fmt::format
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration
 };
+use rust_server::ThreadPool;
 
 
 fn main() {
     let listener = TcpListener::bind("localhost:3000").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream)
+        pool.execute(|| {
+            handle_connection(stream)
+        }); 
     }
 }
 
@@ -28,6 +34,7 @@ fn handle_connection (mut stream: TcpStream) {
     let req_line = buf_reader.lines().next().unwrap().unwrap();
 
     if req_line == "GET / HTTP/1.1" {
+        thread::sleep(Duration::from_secs(5));
         send_page("HTTP/1.1 200 OK", "index.html", stream);
     } else {
         send_page("HTTP/1.1 404 Not Found", "404.html", stream);
